84016034887cdaeb410aa5ec59bc78d367add2d5
Dmitriy Lyubimov <dlyubimov@inadco.com>
2011-01-19 11:36:54 -0800

    release script mod

--- unrelease.sh
+++ unrelease.sh
@@ -1,6 +1,7 @@
 . ./branch.sh
 
-git checkout $DEV_BRANCH
+git checkout -f $DEV_BRANCH
+git clean -fd
 git tag -d tag-$REL_VERSION
 git branch -D $REL_BRANCH-$REL_VERSION
 git push $REMOTE :$REL_BRANCH-$REL_VERSION
