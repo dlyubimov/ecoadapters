1c3e127fddd4fef6a85f0bc587a25bea5d436c65
Dmitriy Lyubimov <dlyubimov@inadco.com>
2011-08-19 15:58:49 -0700

    changing env name for repo not to collide with inadco's repos

--- deploy-snapshot.sh
+++ deploy-snapshot.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-MVN_RELEASE_REPO=releases::default::file:../dlyubimov-maven-repo/releases    
-MVN_SNAPSHOT_REPO=snapshots::default::file:../dlyubimov-maven-repo/snapshots
-
-mvn -e -Dmaven.test.skip=true -DaltDeploymentRepository=$MVN_SNAPSHOT_REPO clean install deploy
+mvn -e -Dmaven.test.skip=true -DaltDeploymentRepository=$MVN_GITHUB_SNAPSHOT_REPO clean install deploy
 
