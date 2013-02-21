0a65f30a2448b9d12e993828f5bf1ac2633b5b40
Dmitriy Lyubimov <dlyubimov@apache.org>
2013-02-20 17:26:09 -0800

    reformatting, warnings

--- ecoadapters/src/main/java/com/inadco/ecoadapters/cascading/ProtoScheme.java
+++ ecoadapters/src/main/java/com/inadco/ecoadapters/cascading/ProtoScheme.java
@@ -41,41 +41,47 @@ import java.io.IOException;
 
 /**
  * Cascading Protobuf Scheme for reading/writing sequence files with protobuf
- * payload. <P/>
- *
- * Since Cascading doesn't really support notion of nested messages schema,
- * we only roll-out (or roll-in) the first layer of the tuple only, unlike for
- * Pig and Hive. <P>
- *
+ * payload.
+ * <P/>
+ * 
+ * Since Cascading doesn't really support notion of nested messages schema, we
+ * only roll-out (or roll-in) the first layer of the tuple only, unlike for Pig
+ * and Hive.
+ * <P>
+ * 
  * Repeated fields are converted to an array of value types where applicable
- * (e.g. repeated fixed32 -> <code>int[]</code>); repeated messages are converted to
- * {@link Message}<code>[]</code>. <P/>
- *
+ * (e.g. repeated fixed32 -> <code>int[]</code>); repeated messages are
+ * converted to {@link Message}<code>[]</code>.
+ * <P/>
+ * 
  * Nested messages are left intact (i.e. go just as is as {@link Message} type).
- *
+ * 
  * @author Dmitriy
  */
 public final class ProtoScheme extends SequenceFile {
 
+    private static final long                serialVersionUID = -6464285268036509396L;
+
     private transient Descriptors.Descriptor m_msgDesc;
-    private transient CascadingTupleMap m_tupleMap;
-    private transient Text m_outKey = new Text();
-    private String className;
-    
+    private transient CascadingTupleMap      m_tupleMap;
+    private transient Text                   m_outKey         = new Text();
+    private String                           className;
+
     /**
-     * Same message descriptor as with pig and hive etc. adapters.
-     * I.e. either class name or hdfs reference to the descirptor file + ?msg="message".
-     * See  e.g. {@link com.inadco.ecoadapters.pig.SequenceFileProtobufLoader}, or README,
-     * for details.<P/>
-     *
+     * Same message descriptor as with pig and hive etc. adapters. I.e. either
+     * class name or hdfs reference to the descirptor file + ?msg="message". See
+     * e.g. {@link com.inadco.ecoadapters.pig.SequenceFileProtobufLoader}, or
+     * README, for details.
+     * <P/>
+     * 
      * @param msgDescString
      * @throws IOException
      */
     public ProtoScheme(String msgDescString) throws IOException {
         super(new Fields());
-        
+
         className = msgDescString;
-        
+
         try {
             if (msgDescString.startsWith("hdfs://"))
                 m_msgDesc = EcoUtil.inferDescriptorFromFilesystem(msgDescString);
@@ -92,37 +98,41 @@ public final class ProtoScheme extends SequenceFile {
         setSinkFields(new Fields(fnames));
 
     }
-    
+
     private void init() {
-    	if(m_msgDesc == null) {
-    		try {
-            	if (className.startsWith("hdfs://"))
-                	m_msgDesc = EcoUtil.inferDescriptorFromFilesystem(className);
-            	else
-            		m_msgDesc = EcoUtil.inferDescriptorFromClassName(className);
-        	} catch (Throwable e) {
-        		throw new RuntimeException("Error initializing ProtoScheme ",e);
-        	}
-        	m_tupleMap = new CascadingTupleMap(m_msgDesc);
-        	String[] fnames = m_tupleMap.getFieldNames();
-        	m_outKey = new Text();
-        
-        	setSourceFields(new Fields(fnames));
-        	setSinkFields(new Fields(fnames));
-    	}
+        if (m_msgDesc == null) {
+            try {
+                if (className.startsWith("hdfs://"))
+                    m_msgDesc = EcoUtil.inferDescriptorFromFilesystem(className);
+                else
+                    m_msgDesc = EcoUtil.inferDescriptorFromClassName(className);
+            } catch (Throwable e) {
+                throw new RuntimeException("Error initializing ProtoScheme ", e);
+            }
+            m_tupleMap = new CascadingTupleMap(m_msgDesc);
+            String[] fnames = m_tupleMap.getFieldNames();
+            m_outKey = new Text();
+
+            setSourceFields(new Fields(fnames));
+            setSinkFields(new Fields(fnames));
+        }
     }
 
     @Override
+    @SuppressWarnings("rawtypes")
     public void sourcePrepare(FlowProcess<JobConf> flowProcess, SourceCall<Object[], RecordReader> sourceCall) {
         super.sourcePrepare(flowProcess, sourceCall);
         init();
         if (!(sourceCall.getContext()[1] instanceof BytesWritable))
             throw new RuntimeException(
-                    "Cascading adapter for protobuff sequence files requires BytesWritable as file value type");
+                "Cascading adapter for protobuff sequence files requires BytesWritable as file value type");
     }
 
     @Override
-    public void sinkConfInit(FlowProcess<JobConf> flowProcess, Tap<JobConf, RecordReader, OutputCollector> tap, JobConf conf) {
+    @SuppressWarnings("rawtypes")
+    public void sinkConfInit(FlowProcess<JobConf> flowProcess,
+                             Tap<JobConf, RecordReader, OutputCollector> tap,
+                             JobConf conf) {
         super.sinkConfInit(flowProcess, tap, conf);
         init();
         conf.setOutputKeyClass(Text.class);
@@ -130,15 +140,18 @@ public final class ProtoScheme extends SequenceFile {
     }
 
     @Override
-    @SuppressWarnings("unchecked")
-    public void sinkPrepare(FlowProcess<JobConf> flowProcess, SinkCall<Void, OutputCollector> sinkCall) throws IOException {
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public void sinkPrepare(FlowProcess<JobConf> flowProcess, SinkCall<Void, OutputCollector> sinkCall)
+        throws IOException {
         super.sinkPrepare(flowProcess, sinkCall);
         init();
         ((SinkCall) sinkCall).setContext(new BytesWritable());
     }
 
     @Override
-    public boolean source(FlowProcess<JobConf> flowProcess, SourceCall<Object[], RecordReader> sourceCall) throws IOException {
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public boolean source(FlowProcess<JobConf> flowProcess, SourceCall<Object[], RecordReader> sourceCall)
+        throws IOException {
         Writable key = (Writable) sourceCall.getContext()[0];
         BytesWritable bw = (BytesWritable) sourceCall.getContext()[1];
         boolean result = sourceCall.getInput().next(key, bw);
@@ -147,21 +160,20 @@ public final class ProtoScheme extends SequenceFile {
             return false;
 
         Tuple tuple = sourceCall.getIncomingEntry().getTuple();
-        DynamicMessage msg = DynamicMessage.parseFrom(m_msgDesc,
-                CodedInputStream.newInstance(bw.getBytes(), 0, bw.getLength()));
+        DynamicMessage msg =
+            DynamicMessage.parseFrom(m_msgDesc, CodedInputStream.newInstance(bw.getBytes(), 0, bw.getLength()));
         m_tupleMap.proto2T(msg, tuple);
         return true;
     }
 
     @Override
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     public void sink(FlowProcess<JobConf> flowProcess, SinkCall<Void, OutputCollector> sinkCall) throws IOException {
         Tuple t = sinkCall.getOutgoingEntry().getTuple();
         Message.Builder b = m_tupleMap.t2proto(t);
         byte[] msgBytes = b.build().toByteArray();
-        @SuppressWarnings({"unchecked", "rawtypes"})
         BytesWritable val = (BytesWritable) ((SinkCall) sinkCall).getContext();
         val.set(msgBytes, 0, msgBytes.length);
         sinkCall.getOutput().collect(m_outKey, val);
     }
 }
-
